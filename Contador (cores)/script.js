d9d5d9230d81e79602f093ca697480c66993782b
Timóteo Júnior <112556813+timoteo-junior@users.noreply.github.com>
2023-06-06 10:52:09 -0300

    desafio de cores
    agora tem botão de reset, quando o valor for < 0, ficará vermelho

---
+++ Contador (cores)/script.js
@@ -0,0 +1,30 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+const adicionar = document.getElementById("adicionar");
+
+adicionar.addEventListener ('click', function(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber >= 0){
+        currentNumberWrapper.style.color = 'white';
+    }
+    
+});
+
+const subtrair = document.getElementById("subtrair");
+
+subtrair.addEventListener ('click', function(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
+});
+
+const reset = document.getElementById("reset");
+
+reset.addEventListener ('click', function(){
+    currentNumber = 0;
+    currentNumberWrapper.innerHTML = currentNumber;
+});
